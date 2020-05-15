e31d24ab17383125a1b707f3b9d3fcdd9f15f0ff
Adam Belebczuk <adam@sqldiablo.com>
2020-05-15 16:58:05 -0400

    Even more fun with ActionException

--- homeassistant/components/wemo/fan.py
+++ homeassistant/components/wemo/fan.py
@@ -4,7 +4,7 @@ from datetime import timedelta
 import logging
 
 import async_timeout
-import pywemo.ouimeaux_device.ActionException as ActionException
+from pywemo.ouimeaux_device.api.service import ActionException
 import voluptuous as vol
 
 from homeassistant.components.fan import (
